21481e5071a8400d2c8bd96db59f9e2b668f0f24
Sambit Acharya <sambitacharya121@gmail.com>
2015-01-10 20:39:15 +0530

    Added test data

--- Sorting/selectionsort.py
+++ Sorting/selectionsort.py
@@ -9,3 +9,6 @@ def selectionSort(alist):
        alist[fillslot] = alist[positionOfMax]
        alist[positionOfMax] = temp
 
+alist = [54,26,93,17,77,31,44,55,20]
+selectionSort(alist)
+print(alist)
