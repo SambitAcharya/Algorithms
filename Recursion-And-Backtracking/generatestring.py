dfb52575f6696401e9d6c0183bce25ce6d0b4ee7
Sambit Acharya <sambitacharya121@gmail.com>
2015-01-13 18:21:30 +0530

    Updated generatestring.py

--- Recursion-And-Backtracking/generatestring.py
+++ Recursion-And-Backtracking/generatestring.py
@@ -1,12 +1,14 @@
-A = [1,2,3,4,5]
-
 def Binary(n):
     if n<1:
-        print A
+        print A # Printing the whole string
     else:
         A[n-1] = 0
         Binary(n-1)
         A[n-1] = 1
         Binary(n-1)
 
-Binary(5)
+n = input("Enter the number of bits. \n")
+
+A = [0 for x in range(n)] # Generating an empty list.
+
+Binary(n) # Calling the function to generate the Binary
