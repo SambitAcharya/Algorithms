75db2af91951921e2702033fc16c92391bbd29b8
Sambit Acharya <sambitacharya121@gmail.com>
2015-01-28 15:54:05 +0530

    Update knapsack.py

--- Dynamic-Programming/knapsack.py
+++ Dynamic-Programming/knapsack.py
@@ -1,7 +1,12 @@
-def knapsack():
-    return 0
+def max(a,b):
+    return a if a>b else b
 
-def main()
+def knapsack():
+    for i in range(n+1):
+        for j in range(w+1):
+            print l[i][j]
+
+def main():
     knapsack()
     
 main()
