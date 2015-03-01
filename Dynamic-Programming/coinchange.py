794d35f46dbe0658a98f4bd6993da119ab8f71c8
Sambit Acharya <sambitacharya121@gmail.com>
2015-03-02 00:18:26 +0530

    Update coinchange.py

--- Dynamic-Programming/coinchange.py
+++ Dynamic-Programming/coinchange.py
@@ -21,3 +21,18 @@ def printCoins(coinsUsed,change):
       thisCoin = coinsUsed[coin]
       print(thisCoin)
       coin = coin - thisCoin
+
+def main():
+    amnt = 63
+    clist = [1,5,10,21,25]
+    coinsUsed = [0]*(amnt+1)
+    coinCount = [0]*(amnt+1)
+
+    print("Making change for",amnt,"requires")
+    print(dpMakeChange(clist,amnt,coinCount,coinsUsed),"coins")
+    print("They are:")
+    printCoins(coinsUsed,amnt)
+    print("The used list is as follows:")
+    print(coinsUsed)
+
+main()
